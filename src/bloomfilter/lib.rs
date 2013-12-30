1f9452e4987dfa19e620182c57630ec687788e80
Frank Denis <github@pureftpd.org>
2013-12-30 12:11:03 +0100

    Use #[crate_id]

--- src/bloomfilter/lib.rs
+++ src/bloomfilter/lib.rs
@@ -9,7 +9,7 @@
 
 #[desc = "A fast Bloom filter implementation."];
 #[license = "BSD"];
-#[link(name = "bloomfilter", vers = "0.1")];
+#[crate_id="bloomfilter#0.1"];
 #[crate_type = "lib"];
 #[warn(non_camel_case_types, non_uppercase_statics, non_uppercase_statics, unnecessary_qualification, managed_heap_memory)];
 
