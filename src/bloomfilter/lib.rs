d0f99701436779db1147a63e043d2772b2e11396
Frank Denis <github@pureftpd.org>
2015-01-10 23:11:16 +0100

    uint -> usize

--- src/bloomfilter/lib.rs
+++ src/bloomfilter/lib.rs
@@ -29,7 +29,7 @@ use std::rand::Rng;
 pub struct Bloom {
     bitmap: bitv::Bitv,
     bitmap_bits: u64,
-    k_num: uint,
+    k_num: u32,
     sips: [SipHasher; 2]
 }
 
@@ -37,11 +37,11 @@ impl Bloom {
 /// Create a new bloom filter structure.
 /// bitmap_size is the size in bytes (not bits) that will be allocated in memory
 /// items_count is an estimation of the maximum number of items to store.
-    pub fn new(bitmap_size: uint, items_count: uint) -> Bloom {
-        assert!(bitmap_size > 0u && items_count > 0u);
+    pub fn new(bitmap_size: usize, items_count: usize) -> Bloom {
+        assert!(bitmap_size > 0 && items_count > 0);
         let bitmap_bits = (bitmap_size as u64) * 8u64;
         let k_num = Bloom::optimal_k_num(bitmap_bits, items_count);
-        let bitmap = bitv::Bitv::from_elem(bitmap_bits as uint, false);
+        let bitmap = bitv::Bitv::from_elem(bitmap_bits as usize, false);
         let sips = [ Bloom::sip_new(), Bloom::sip_new() ];
         Bloom {
             bitmap: bitmap,
@@ -54,7 +54,7 @@ impl Bloom {
 /// Create a new bloom filter structure.
 /// items_count is an estimation of the maximum number of items to store.
 /// fp_p is the wanted rate of false positives, in ]0.0, 1.0[
-    pub fn new_for_fp_rate(items_count: uint, fp_p: f64) -> Bloom {
+    pub fn new_for_fp_rate(items_count: usize, fp_p: f64) -> Bloom {
         let bitmap_size = Bloom::compute_bitmap_size(items_count, fp_p);
         Bloom::new(bitmap_size, items_count)
     }
@@ -62,31 +62,31 @@ impl Bloom {
 /// Compute a recommended bitmap size for items_count items
 /// and a fp_p rate of false positives.
 /// fp_p obviously has to be within the ]0.0, 1.0[ range.
-    pub fn compute_bitmap_size(items_count: uint, fp_p: f64) -> uint {
-        assert!(items_count > 0u);
+    pub fn compute_bitmap_size(items_count: usize, fp_p: f64) -> usize {
+        assert!(items_count > 0);
         assert!(fp_p > 0.0 && fp_p < 1.0);
         let log2 = f64::consts::LN_2;
         let log2_2 = log2 * log2;
-        ((items_count as f64) * Float::ln(fp_p) / (-8.0 * log2_2)).ceil() as uint
+        ((items_count as f64) * Float::ln(fp_p) / (-8.0 * log2_2)).ceil() as usize
     }
 
 /// Record the presence of an item.
-    pub fn set<T: Hash>(& mut self, item: T) {
+    pub fn set<T>(& mut self, item: T) {
         let mut hashes = [ 0u64, 0u64 ];
-        for k_i in range(0u, self.k_num) {
+        for k_i in range(0, self.k_num) {
             let bit_offset = (self.bloom_hash(& mut hashes, &item, k_i)
-                              % self.bitmap_bits) as uint;
+                              % self.bitmap_bits) as usize;
             self.bitmap.set(bit_offset, true);
         }
     }
 
 /// Check if an item is present in the set.
 /// There can be false positives, but no false negatives.
-    pub fn check<T: Hash>(&self, item: T) -> bool {
+    pub fn check<T>(&self, item: T) -> bool {
         let mut hashes = [ 0u64, 0u64 ];
-        for k_i in range(0u, self.k_num) {
+        for k_i in range(0, self.k_num) {
             let bit_offset = (self.bloom_hash(& mut hashes, &item, k_i)
-                              % self.bitmap_bits) as uint;
+                              % self.bitmap_bits) as usize;
             if self.bitmap.get(bit_offset).unwrap() == false {
                 return false;
             }
@@ -96,12 +96,12 @@ impl Bloom {
 
 /// Record the presence of an item in the set,
 /// and return the previous state of this item.
-    pub fn check_and_set<T: Hash>(&mut self, item: T) -> bool {
+    pub fn check_and_set<T>(&mut self, item: T) -> bool {
         let mut hashes = [ 0u64, 0u64 ];
         let mut found = true;
-        for k_i in range(0u, self.k_num) {
+        for k_i in range(0, self.k_num) {
             let bit_offset = (self.bloom_hash(& mut hashes, &item, k_i)
-                              % self.bitmap_bits) as uint;
+                              % self.bitmap_bits) as usize;
             if self.bitmap.get(bit_offset).unwrap() == false {
                 found = false;
                 self.bitmap.set(bit_offset, true);
@@ -116,23 +116,23 @@ impl Bloom {
     }
 
 /// Return the number of hash functions used for `check` and `set` 
-    pub fn number_of_hash_functions(&self) -> uint {
+    pub fn number_of_hash_functions(&self) -> u32 {
         self.k_num
     }
 
-    fn optimal_k_num(bitmap_bits: u64, items_count: uint) -> uint {
+    fn optimal_k_num(bitmap_bits: u64, items_count: usize) -> u32 {
         let m = bitmap_bits as f64;
         let n = items_count as f64;
-        let k_num = (m / n * Float::ln(2.0f64)).ceil() as uint;
+        let k_num = (m / n * Float::ln(2.0f64)).ceil() as u32;
         cmp::max(k_num, 1)
     }
 
-    fn bloom_hash<T: Hash>(&self, hashes: & mut [u64; 2],
-                  item: &T, k_i: uint) -> u64 {
+    fn bloom_hash<T>(&self, hashes: & mut [u64; 2],
+                  item: &T, k_i: u32) -> u64 {
         if k_i < 2 {
-            let sip = &self.sips[k_i];
+            let sip = &self.sips[k_i as usize];
             let hash = sip.hash(item);
-            hashes[k_i] = hash;
+            hashes[k_i as usize] = hash;
             hash
         } else {
             hashes[0] + (((k_i as u64) * hashes[1]) % 0xffffffffffffffc5)
@@ -149,7 +149,7 @@ impl Bloom {
 #[test]
 fn bloom_test_set() {
     let mut bloom = Bloom::new(10, 80);
-    let key: &Vec<u8> = &rand::task_rng().gen_iter::<u8>().take(16u).collect();
+    let key: &Vec<u8> = &rand::task_rng().gen_iter::<u8>().take(16).collect();
     assert!(bloom.check(key) == false);
     bloom.set(&key);
     assert!(bloom.check(key.clone()) == true);
@@ -158,7 +158,7 @@ fn bloom_test_set() {
 #[test]
 fn bloom_test_check_and_set() {
     let mut bloom = Bloom::new(10, 80);
-    let key: &Vec<u8> = &rand::task_rng().gen_iter::<u8>().take(16u).collect();
+    let key: &Vec<u8> = &rand::task_rng().gen_iter::<u8>().take(16).collect();
     assert!(bloom.check_and_set(key) == false);
     assert!(bloom.check_and_set(key.clone()) == true);
 }
