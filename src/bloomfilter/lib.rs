7b8662fe72c4bbead43e88dd2987e0f72dfb6c10
Frank Denis <github@pureftpd.org>
2015-01-31 13:32:02 +0100

    +#![feature(rand, core, hash, collections)]

--- src/bloomfilter/lib.rs
+++ src/bloomfilter/lib.rs
@@ -10,7 +10,7 @@
 #![crate_name="bloomfilter"]
 #![crate_type = "rlib"]
 #![warn(non_camel_case_types, non_upper_case_globals, unused_qualifications)]
-#![allow(unstable)]
+#![feature(rand, core, hash, collections)]
 
 extern crate collections;
 
