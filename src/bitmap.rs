a997fe5a07c1c3e390eff0a07a82939765d9dea1
Frank Denis <github@pureftpd.org>
2024-12-02 14:50:38 +0100

    Force inline a few things

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -19,50 +19,62 @@ impl BitMap {
         Self { header_and_bits }
     }
 
+    #[inline]
     fn bits(&self) -> &[u8] {
         &self.header_and_bits[BITMAP_HEADER_SIZE..]
     }
 
+    #[inline]
     fn bits_mut(&mut self) -> &mut [u8] {
         &mut self.header_and_bits[BITMAP_HEADER_SIZE..]
     }
 
+    #[inline]
     pub fn header(&self) -> &[u8] {
         &self.header_and_bits[0..BITMAP_HEADER_SIZE]
     }
 
+    #[inline]
     pub fn header_mut(&mut self) -> &mut [u8] {
         &mut self.header_and_bits[0..BITMAP_HEADER_SIZE]
     }
 
+    #[inline]
     fn get_version(header: &[u8]) -> u8 {
         header[0]
     }
 
+    #[inline]
     fn set_version(header: &mut [u8], version: u8) {
         header[0] = version;
     }
 
+    #[inline]
     fn get_len_bytes(header: &[u8]) -> u64 {
         u64::from_le_bytes(header[1..][0..8].try_into().unwrap())
     }
 
+    #[inline]
     fn set_len_bytes(header: &mut [u8], len_bytes: u64) {
         header[1..][0..8].copy_from_slice(&len_bytes.to_le_bytes());
     }
 
+    #[inline]
     pub fn get_k_num(header: &[u8]) -> u32 {
         u32::from_le_bytes(header[9..][0..4].try_into().unwrap())
     }
 
+    #[inline]
     pub fn set_k_num(header: &mut [u8], k_num: u32) {
         header[9..][0..4].copy_from_slice(&k_num.to_le_bytes());
     }
 
+    #[inline]
     pub fn get_seed(header: &[u8]) -> [u8; 32] {
         header[13..][0..32].try_into().unwrap()
     }
 
+    #[inline]
     pub fn set_seed(header: &mut [u8], seed: &[u8; 32]) {
         header[13..][0..32].copy_from_slice(seed);
     }
@@ -113,14 +125,17 @@ impl BitMap {
         Ok(res)
     }
 
+    #[inline]
     pub fn as_slice(&self) -> &[u8] {
         &self.header_and_bits
     }
 
+    #[inline]
     pub fn into_bytes(self) -> Vec<u8> {
         self.header_and_bits
     }
 
+    #[inline]
     pub fn to_bytes(&self) -> Vec<u8> {
         self.header_and_bits.clone()
     }
